e1fcc2027d16f30b71966c1e51e40ce18cfcdb0b
karrocks <8guptakartik@gmail.com>
2022-02-01 02:51:18 +0530

    Edit Text se input leke button dabake niche aa raha

---
+++ app/src/main/java/academy/learnprogramming/buttonclickapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package academy.learnprogramming.buttonclickapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private var userInput : EditText? = null
+    private var button : Button? = null
+    private var textView : TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        userInput = findViewById<EditText>(R.id.editText)
+        button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        button?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+
+                textView?.append(userInput?.text)
+                textView?.append("\n")
+
+
+            }
+        })
+    }
+
+
+}
